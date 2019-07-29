85b3a89433e2eea97c23b33153cbce5771f4281a
lijun.zhou <zhouj.172@gmail.com>
2019-07-29 10:27:46 +0800

    修改默认配置文件

--- src/main/java/io/nuls/MyModule.java
+++ src/main/java/io/nuls/MyModule.java
@@ -2,8 +2,12 @@ package io.nuls;
 
 import io.nuls.core.core.annotation.Autowired;
 import io.nuls.core.core.annotation.Component;
+import io.nuls.core.core.annotation.Value;
+import io.nuls.core.exception.NulsException;
+import io.nuls.core.log.Log;
 import io.nuls.core.rpc.modulebootstrap.Module;
 import io.nuls.core.rpc.modulebootstrap.RpcModuleState;
+import io.nuls.rpctools.AccountTools;
 import io.nuls.rpctools.TransactionTools;
 
 /**
@@ -17,6 +21,9 @@ public class MyModule {
     @Autowired
     TransactionTools transactionTools;
 
+    @Autowired
+    AccountTools accountTools;
+
     /**
      * 启动模块
      * 模块启动后，当申明的依赖模块都已经准备就绪将调用此函数
@@ -24,8 +31,7 @@ public class MyModule {
      * @return
      */
     public RpcModuleState startModule(String moduleName){
-        //注册交易
-        //transactionTools.registerTx(moduleName,200);
+        //todo
         return RpcModuleState.Running;
     }
 
